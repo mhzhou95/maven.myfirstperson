a7687922aec5e73ed948d0c9ee4f7ef2bd40a84f
unknown <minghaozhou123@gmail.com>
2019-12-18 11:36:10 -0500

    all tests passed

--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -4,22 +4,32 @@ package com.github.curriculeon;
  * Created by leon on 12/16/2019.
  */
 public class Person {
-    public Person(){}
+    private String firstName;
+    private String lastName;
+
+    public Person(){
+        this.lastName = "";
+        this.firstName = "";
+    }
 
     public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
     public String getFirstName() {
-        return null;
+        return this.firstName;
     }
 
     public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
     public String getLastName() {
-        return null;
+        return this.lastName;
     }
 
     public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
